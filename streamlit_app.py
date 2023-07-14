42804c927036557de0d11b3c5a5e96322dbedfd7
trainanth <139361660+trainanth@users.noreply.github.com>
2023-07-14 15:26:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,12 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
+fruit_choice = streamlit.text_input('What ruit would you like information about?','Kiwi')
+streamlit.write('The user entered, fruit_choice)
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment -what does the next line do? 
